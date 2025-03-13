cd73393b9e8f9153d077ca40faab2cc0a43d67ee
Knightrider8611 <rathanon.makaramanee@gmail.com>
2025-03-13 21:26:09 +0700

    Retry autograding system.

--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import styles from "./topmenu.module.css"
+
 export default function TopMenuItem({title,pageRef}:{title:string,pageRef:string}){
     return (
         <Link href={pageRef} className={styles.itemcontainer}>
